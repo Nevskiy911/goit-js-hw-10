97283b0de2b90c2a734da5a65c4bdce92edca0cc
Nevskiy911 <122637230+Nevskiy911@users.noreply.github.com>
2025-02-08 13:39:16 +0100

    Initialize gh-pages

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DKKyP-z-.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector('a[href="/1-gallery.html"]'),t=document.querySelector('a[href="/2-form.html"]');e.addEventListener("click",o=>{console.log("Redirecting to Gallery")}),t.addEventListener("click",o=>{console.log("Redirecting to User Form")})});
+//# sourceMappingURL=index.js.map
