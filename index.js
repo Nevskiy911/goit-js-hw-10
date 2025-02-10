b0feeedacf59e5bcd20230916f728e3de97b51a0
Nevskiy911 <122637230+Nevskiy911@users.noreply.github.com>
2025-02-10 19:27:06 +0000

    Deploying to gh-pages from @ Nevskiy911/goit-js-hw-10@b05e509df6adab2444d47c5a18f462b93d0f3118 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DKKyP-z-.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector('a[href="/1-gallery.html"]'),t=document.querySelector('a[href="/2-form.html"]');e.addEventListener("click",o=>{console.log("Redirecting to Gallery")}),t.addEventListener("click",o=>{console.log("Redirecting to User Form")})});
+import"./assets/styles-DTzD-3L3.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector('a[href="/1-timer.html"]'),t=document.querySelector('a[href="/2-snackbar.html"]');e.addEventListener("click",n=>{console.log("Redirecting to Gallery")}),t.addEventListener("click",n=>{console.log("Redirecting to User Form")})});
 //# sourceMappingURL=index.js.map
